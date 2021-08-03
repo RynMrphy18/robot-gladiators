54abd57ebd02bf90b030444409dcecade4018547
Ryan <rmurph1800@gmail.com>
2021-08-03 17:54:12 -0400

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -77,10 +77,21 @@ var fight = function(enemyName) {
 
 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(enemyNames[i]);
-}
+  if(playerHealth > 0) {
+    
+    window.alert("Welcome to the Robot Gladiators! Round " + (i + 1 ) );
 
+    var pickedEnemyName = enemyNames[i];
+
+    enemyHealth = 50;
+
+    debugger;
+
+    fight(enemyNames[i]);
+} else {
+  window.alert("Get fucked loser lol");
+  break;
+}
+}
 
 
